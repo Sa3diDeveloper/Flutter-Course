729e40be4411e200951b9ba4917a79b9e15ce599
Sa3di Progarmmer <125812723+Sa3diDeveloper@users.noreply.github.com>
2023-03-14 00:25:24 +0300

    Delete applifecycle directory

--- applifecycle/android/app/src/main/kotlin/com/example/applifecycle/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.applifecycle
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- applifecycle/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:applifecycle/applifecycle/app_life_cycle.dart';
-import 'package:applifecycle/widgets_lifecycle.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp( WidgetsAppLIfeCycle());
-}
-
-
