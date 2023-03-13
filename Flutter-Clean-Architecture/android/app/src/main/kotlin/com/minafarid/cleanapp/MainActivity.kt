9b3bfe1d58505a61aa1d5a3f7cfe32c23d02212c
Sa3di Progarmmer <125812723+Sa3diDeveloper@users.noreply.github.com>
2023-03-14 00:01:40 +0300

    Delete Flutter-Clean-Architecture directory

--- Flutter-Clean-Architecture/android/app/src/main/kotlin/com/minafarid/cleanapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.minafarid.cleanapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter-Clean-Architecture/lib/domain/model.dart
+++
@@ -1,6 +0,0 @@
-class SlideObjects {
-  String title;
-  String subtitle;
-  String iamge;
-  SlideObjects(this.title, this.subtitle, this.iamge);
-}
