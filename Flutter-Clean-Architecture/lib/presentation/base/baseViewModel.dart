4c66fde8687c2198199505d6135e24953a63f6a9
Sa3di Programmer <sa3dopro1231239@gmail.com>
2023-03-15 11:16:03 +0300

    Flutetr Clean Architeture

---
+++ Flutter-Clean-Architecture/lib/presentation/base/baseViewModel.dart
@@ -0,0 +1,11 @@
+
+abstract class BaseViewModel extends BaseViewModelInputs with BaseViewModelOutPuts{
+  
+}
+
+abstract class BaseViewModelInputs {
+  void start();
+  void dispose();
+}
+
+abstract class BaseViewModelOutPuts {}
